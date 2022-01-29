f6800418ef9ef9669c7a6a8ae197d9896576b7b5
승철 차 <cscculture95@gmail.com>
2022-01-29 19:25:03 +0900

    씬 매니저 및 오브젝트 작업 시작

---
+++ FroKEngine/Scene/Scene.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "../Game.h"
+
+using namespace std;
+
+class Scene
+{
+public:
+	/* 레이어와 관련된 메소드 */
+	class Layer* CreateLayer(const string& strTag,
+		int nZOrder = 0);
+	class Layer* FindLayer(const string& strTag);
+
+public:
+	// 전역 함수이다!!!!(list의 sort caller가 callee로 전역함수를 원한다.)
+	static bool LayerSort(class CLayer* pL1, class CLayer* pL2);
+
+public:
+	/* 게임 요소를 위한 메소드 */
+	virtual bool Init();
+	// 각 씬마다의 고유한 기능을 넣기 위한 메소드다.
+	virtual void Input(float fDeltaTime);
+	virtual int Update(float fDeltaTime);
+	virtual int LateUpdate(float fDeltaTime);
+	virtual void Collision(float fDeltaTime);
+	virtual void Render(float fDeltaTime);
+
+public:
+	// 프로토타입 관련 메소드
+	static void ErasePrototype(SCENE_CREATE sc);	// 전체
+	static void ErasePrototype(const string& strTag, SCENE_CREATE sc);
+
+	// 프로토타입 제작 메소드
+	template<typename T>
+	T* CreatePrototype(const string& strTag, SCENE_CREATE sc)
+	{
+		T* pObj = new T;
+
+		pObj->SetTag(strTag);
+
+		if (!pObj->Init())
+		{
+			SAFE_RELEASE(pObj);
+			return nullptr;
+		}
+
+		pObj->AddRef();
+		m_mapPrototype[sc].insert(make_pair(strTag, pObj));
+
+		return pObj;
+	}
+
+protected:
+	friend class SceneManager;	// 씬 매니저만 이 클래스에 접근 가능하다.
+
+protected:
+	Scene();
+	virtual ~Scene() = 0;	// 순수 가상함수로!
+
+
+// 오브젝트 자체를 레이어에 배치할 것이다.
+// 레이어는 리스트로 보관하자.
+protected:
+	// 이 방식을 이용하면 장면이 날아가면 그 장면에 종속된 모든
+	// 레이어들도 함께 날아간다.
+	list<class CLayer*> m_LayerList;
+	SCENE_CREATE		m_eSceneType;
+
+public:
+	void SetSceneType(SCENE_CREATE eType)
+	{
+		m_eSceneType = eType;
+	}
+
+	SCENE_CREATE GetSceneType() const
+	{
+		return m_eSceneType;
+	}
+
+public:
+	// 생성할 때만 필요하다.
+	static class GameObject* FindPrototype(const string& strTag, SCENE_CREATE sc);
+	static void ChangePrototype();
+
+private:
+	// 원본 객체(프로토타입)을 관리
+	// 복사할 용도, 
+	// 예로들어 오크를 만든다 치면
+	// HP 등등의 정보가 있을 것이다.
+	// 근데 문제는 모든 몬스터는 정보가 다를 것이다.
+	// 그러면 파일에 있는 것을 로딩할텐데 문제는 느려!
+	// 그렇기 때문에 여기다가 미리 만들어놓고, 리스폰될 객체들을
+	// 만들면 될 것이다.
+	static unordered_map<string, class GameObject*> m_mapPrototype[SC_END];
+};
---
+++ FroKEngine/Scene/SceneManager.cpp
@@ -0,0 +1,72 @@
+#include "SceneManager.h"
+
+DEFINITION_SINGLE(SceneManager)
+
+// m_pScene : 현재 씬을 나타낸다.
+SceneManager::SceneManager() :
+	m_pScene(nullptr),
+	m_pNextScene(nullptr)
+{
+
+}
+
+SceneManager::~SceneManager()
+{
+	SAFE_DELETE(m_pScene);
+}
+
+// 서브 관리자로써 할 일들을 준다.
+bool SceneManager::Init()
+{
+	// 기본 장면은 무조건 하나 만들어둔다.
+	// CreateScene<CStartScene>(SC_CURRENT);
+
+	return true;
+}
+
+void SceneManager::Input(float fDeltaTime)
+{
+	// 현재 씬의 입력을 받는다.
+	// m_pScene->Input(fDeltaTime);
+}
+
+SCENE_CHANGE SceneManager::Update(float fDeltaTime)
+{
+	m_pScene->Update(fDeltaTime);
+	return ChangeScene();
+}
+
+SCENE_CHANGE SceneManager::LateUpdate(float fDeltaTime)
+{
+	m_pScene->LateUpdate(fDeltaTime);
+	return ChangeScene();
+}
+
+void SceneManager::Collision(float fDeltaTime)
+{
+	m_pScene->Collision(fDeltaTime);
+}
+
+void SceneManager::Render(float fDeltaTime)
+{
+	// m_pScene->Render(fDeltaTime);
+}
+
+SCENE_CHANGE SceneManager::ChangeScene()
+{
+	if (m_pNextScene)
+	{
+		SAFE_DELETE(m_pScene);
+		m_pScene = m_pNextScene;
+		m_pNextScene = nullptr;
+
+		GET_SINGLE(CCollisionManager)->ClearScene();
+
+		m_pScene->SetSceneType(SC_CURRENT);
+		CScene::ChangePrototype();
+
+		return SC_CHANGE;
+	}
+
+	return SC_NONE;
+}
